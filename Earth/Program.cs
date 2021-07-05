6ee72df989e0379ec4b935ea1532aa42ed2f4130
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-05 12:21:27 +0400

    client socket start point - earth

---
+++ Earth/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Earth.Extensions;
+
+namespace Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client (Earth)";
+            SocketClient.SendContinuousMessages();
+        }
+    }
+}
