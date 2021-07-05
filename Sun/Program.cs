761ebac1bebd67975bb01c216916db7fe3066a7c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-05 12:21:44 +0400

    listening server socket start point - sun

---
+++ Sun/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using Sun.Extensions;
+
+namespace Sun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Listener (Sun)";
+            Thread.Sleep(300);
+            SocketListener.Start();
+        }
+    }
+}
