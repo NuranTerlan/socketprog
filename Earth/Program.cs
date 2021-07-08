dd2d217fac3d680a1e85c4f899d537d0879a9d40
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-08 14:50:12 +0400

    client enter it's input manually when client socket program is started

--- Earth/Program.cs
+++ Earth/Program.cs
@@ -10,8 +10,11 @@ namespace Earth
         static async Task Main(string[] args)
         {
             Console.Title = "Client (Earth)";
-            await Task.Delay(300);
-            await SocketClient.SendMessageAsync();
+            await Task.Delay(100);
+            Console.Write("Port of instance: ");
+            var port = int.Parse(Console.ReadLine() ?? "1918");
+            var client = new SocketClient(port);
+            await client.SendMessageAsync();
         }
     }
 }
