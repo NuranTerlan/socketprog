9d4f62f995c805ce89507049ec05c34362b31b70
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-12 15:18:18 +0400

    refactor code

--- Earth/Extensions/SocketClient.cs
+++ Earth/Extensions/SocketClient.cs
@@ -21,19 +21,19 @@ namespace Earth.Extensions
             _localEndPoint = new IPEndPoint(_ipAddress, port);
         }
 
-        public async Task SendMessageAsync()
+        public async Task StartClient()
         {
             try
             {
-                using var sender = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                var sender = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sender.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
                 sender.Bind(_localEndPoint);
                 await sender.ConnectAsync(_remoteEndPoint);
-
                 if (sender.Connected)
                 {
                     Console.WriteLine($"Connected to Sun on {_remoteEndPoint} (Socket-Listener)..\n");
                 }
-                
+
                 var processesList = new List<Task>
                 {
                     {Task.Run(async () => await SendMessageAsync(sender))},
@@ -54,14 +54,16 @@ namespace Earth.Extensions
         {
             while (true)
             {
-                var bytes = new byte[sender.ReceiveBufferSize];
+                if (!IsConnected(sender)) continue;
+
+                var bytes = new byte[128];
                 var bytesReceived = await sender.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
                 var data = Encoding.ASCII.GetString(bytes, 0, bytesReceived);
                 WriteReceived(data);
                 AskForNewMessage();
             }
         }
-        
+
         private async Task SendMessageAsync(Socket sender)
         {
             while (true)
@@ -72,19 +74,48 @@ namespace Earth.Extensions
                     ShowErrorMessage("At least one character required to send a message!");
                     continue;
                 }
+
+
+                try
+                {
+                    if (!sender.Connected)
+                    {
+                        await sender.ConnectAsync(_remoteEndPoint);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
                 var msgBytes = Encoding.ASCII.GetBytes(message);
                 await sender.SendAsync(new ArraySegment<byte>(msgBytes), SocketFlags.None);
                 WriteSent(message);
+                sender.DisconnectAsync(new SocketAsyncEventArgs
+                    {DisconnectReuseSocket = true, });
             }
         }
-        
+
+        private static bool IsConnected(Socket socket)
+        {
+            try
+            {
+                return !(socket.Poll(10, SelectMode.SelectRead) && socket.Available == 0);
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+
         private void AskForNewMessage()
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.Write("Write your message: ");
             Console.ResetColor();
         }
-        
+
         private string ReturnNewMessage()
         {
             AskForNewMessage();
@@ -110,12 +141,12 @@ namespace Earth.Extensions
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(content);
         }
-        
+
         private void ClearCurrentConsoleLine()
         {
             var currentLineCursor = Console.CursorTop;
             Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.WindowWidth)); 
+            Console.Write(new string(' ', Console.WindowWidth));
             Console.SetCursorPosition(0, currentLineCursor);
         }
 
